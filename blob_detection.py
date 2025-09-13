a065ded7269031b28cf70e4f087409d0688d867a
Sharko3048 <amaya.arora@gmail.com>
2025-09-13 13:23:36 +0200

    Update blob_detection.py

--- blob_detection.py
+++ blob_detection.py
@@ -14,4 +14,11 @@ params.minInertiaRatio=0.6
 detector=cv2.SimpleBlobDetector_create(params)
 
 key_points=detector.detect(blobs)
+amountofcircs=len(key_points)
 print(key_points)
+
+final_imagee=cv2.drawKeypoints(blobs,key_points,np.zeros((1,1)),(0,255,0),cv2.DrawMatchesFlags_DRAW_RICH_KEYPOINTS)
+cv2.putText(final_imagee,f"Amount of circles:{amountofcircs}",(10,620),cv2.FONT_HERSHEY_COMPLEX,1,(255,0,0))
+cv2.imshow(".",final_imagee)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
